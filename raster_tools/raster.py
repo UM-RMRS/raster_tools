0eb802cce8619848adc2bc05a7799e705c9b7795
Fred <fredrick.bunt@gmail.com>
2021-11-30 17:51:05 -0700

    Refactor batch parsing and make it compatible with current Raster class

--- raster_tools/raster.py
+++ raster_tools/raster.py
@@ -284,7 +284,7 @@ class Raster:
             self._null_value = nv
         elif type(raster) in IO_UNDERSTOOD_TYPES:
             if is_batch_file(raster):
-                rs = BatchScript(raster).parse().final_raster
+                rs = _parse_batch_script(raster).final_raster
                 self._rs = rs._rs
                 self._mask = rs._mask
                 self._null_value = rs._null_value
@@ -1319,6 +1319,10 @@ def _split_strip(s, delimeter):
     return [si.strip() for si in s.split(delimeter)]
 
 
+def _batch_error(msg, line_no):
+    raise BatchScriptParseError(f"Script Line {line_no}: {msg}")
+
+
 FTYPE_TO_EXT = {
     "TIFF": "tif",
 }
@@ -1338,11 +1342,119 @@ _ESRI_OP_TO_OP = {
     "esriRasterPower": "**",
     "**": "**",
 }
-_ARITHMETIC_OPS_MAP = {}
 _FUNC_PATTERN = re.compile(r"^(?P<func>[A-Za-z]+)\((?P<args>[^\(\)]+)\)$")
 
 
-class BatchScript:
+def _batch_parse_arithmetic(state, args_str, line_no):
+    left_arg, right_arg, op = _split_strip(args_str, ";")
+    op = _ESRI_OP_TO_OP[op]
+    if op not in _ESRI_OP_TO_OP:
+        _batch_error(f"Unknown arithmetic operation {repr(op)}", line_no)
+    op = _ESRI_OP_TO_OP[op]
+    try:
+        left = float(left_arg)
+    except ValueError:
+        left = state.get_raster(left_arg)
+    try:
+        right = float(right_arg)
+    except ValueError:
+        right = state.get_raster(right_arg)
+    return left._binary_arithmetic(right, op)
+
+
+def _batch_parse_null_to_value(state, args_str, line_no):
+    left, *right = _split_strip(args_str, ";")
+    if len(right) > 1:
+        _batch_error("NULLTOVALUE Error: Too many arguments", line_no)
+    value = float(right[0])
+    return state.get_raster(left).replace_null(value)
+
+
+def _batch_parse_remap(state, args_str, line_no):
+    raster, *args = _split_strip(args_str, ";")
+    if len(args) > 1:
+        _batch_error("REMAP Error: Too many argument dividers", line_no)
+    args = args[0]
+    remaps = []
+    for group in _split_strip(args, ","):
+        try:
+            values = [float(v) for v in _split_strip(group, ":")]
+        except ValueError:
+            _batch_error("REMAP Error: values must be numbers", line_no)
+        if len(values) != 3:
+            _batch_error(
+                "REMAP Error: requires 3 values separated by ':'", line_no
+            )
+        left, right, new = values
+        if right <= left:
+            _batch_error(
+                "REMAP Error: the min value must be less than the max value",
+                line_no,
+            )
+        remaps.append((left, right, new))
+    if len(remaps) == 0:
+        _batch_error("REMAP Error: No remap values found", line_no)
+    args = []
+    for group in remaps:
+        args.extend(group)
+    return state.get_raster(raster).remap_range(*args)
+
+
+def _batch_parse_composite(state, args_str, line_no):
+    on_line = f" on line {line_no}"
+    rasters = [state.get_raster(path) for path in _split_strip(args_str, ";")]
+    if len(rasters) < 2:
+        _batch_error(
+            "COMPOSITE Error: at least 2 rasters are required", on_line
+        )
+    # Guarded import to avoid circular imports
+    from raster_tools import band_concat
+
+    return band_concat(rasters)
+
+
+def _batch_parse_save(state, args_str, line_no):
+    # From c# files:
+    #  (inRaster;outName;outWorkspace;rasterType;nodata;blockwidth;blockheight)
+    # nodata;blockwidth;blockheight are optional
+    try:
+        in_rs, out_name, out_dir, type_, *extra = _split_strip(args_str, ";")
+    except ValueError:
+        _batch_error(
+            "SAVEFUNCTIONRASTER Error: Incorrect number of arguments", line_no
+        )
+    n = len(extra)
+    bwidth = None
+    bheight = None
+    nodata = 0
+    if n >= 1:
+        nodata = float(extra[0])
+    if n >= 2:
+        bwidth = int(extra[1])
+    if n == 3:
+        bheight = int(extra[2])
+    if n > 3:
+        _batch_error("SAVEFUNCTIONRASTER Error: Too many arguments", line_no)
+    if type_ not in FTYPE_TO_EXT:
+        _batch_error("SAVEFUNCTIONRASTER Error: Unknown file type", line_no)
+    raster = state.get_raster(in_rs)
+    out_name = os.path.join(out_dir, out_name)
+    ext = FTYPE_TO_EXT[type_]
+    out_name += f".{ext}"
+    return raster.save(out_name, nodata, bwidth, bheight)
+
+
+_FUNC_TO_PARSER = {
+    "ARITHMETIC": _batch_parse_arithmetic,
+    "COMPOSITE": _batch_parse_composite,
+    "NULLTOVALUE": _batch_parse_null_to_value,
+    "OPENRASTER": ...,
+    "REMAP": _batch_parse_remap,
+    "SAVEFUNCTIONRASTER": _batch_parse_save,
+}
+
+
+class _BatchScripParserState:
     def __init__(self, path):
         validate_file(path)
         self.path = os.path.abspath(path)
@@ -1350,164 +1462,7 @@ class BatchScript:
         self.rasters = {}
         self.final_raster = None
 
-    def parse(self):
-        with open(self.path) as fd:
-            lines = fd.readlines()
-        last_raster = None
-        for i, line in enumerate(lines):
-            # Ignore comments
-            line, *_ = _split_strip(line, "#")
-            if not line:
-                continue
-            lh, rh = _split_strip(line, "=")
-            self.rasters[lh] = self._parse_raster(lh, rh, i + 1)
-            last_raster = lh
-        self.final_raster = self.rasters[last_raster]
-        return self
-
-    def _parse_raster(self, dst, expr, line_no):
-        mat = _FUNC_PATTERN.match(expr)
-        if mat is None:
-            raise BatchScriptParseError(
-                f"Could not parse function on line {line_no}"
-            )
-        func = mat["func"].upper()
-        args = mat["args"]
-        raster = None
-        if func == "ARITHMETIC":
-            raster = self._arithmetic_args_to_raster(args, line_no)
-        elif func == "NULLTOVALUE":
-            return self._null_to_value_args_to_raster(args, line_no)
-        elif func == "REMAP":
-            raster = self._remap_args_to_raster(args, line_no)
-        elif func == "COMPOSITE":
-            raster = self._composite_args_to_raster(args, line_no)
-        elif func == "OPENRASTER":
-            raster = Raster(args)
-        elif func == "SAVEFUNCTIONRASTER":
-            raster = self._save_args_to_raster(args, line_no)
-        else:
-            raise BatchScriptParseError(
-                f"Unknown function on line {line_no}: '{func}'"
-            )
-        if raster is None:
-            raise BatchScriptParseError(f"Could not parse line: {line_no}")
-        return raster
-
-    def _arithmetic_args_to_raster(self, args_str, line_no):
-        left_arg, right_arg, op = _split_strip(args_str, ";")
-        op = _ESRI_OP_TO_OP[op]
-        if op not in _ESRI_OP_TO_OP:
-            raise BatchScriptParseError(
-                f"Unknown arithmetic operation on line {line_no}: '{op}'"
-            )
-        op = _ESRI_OP_TO_OP[op]
-        try:
-            left = float(left_arg)
-        except ValueError:
-            left = self._get_raster(left_arg)
-        try:
-            right = float(right_arg)
-        except ValueError:
-            right = self._get_raster(right_arg)
-        return left._binary_arithmetic(right, op)
-
-    def _null_to_value_args_to_raster(self, args_str, line_no):
-        on_line = f" on line {line_no}"
-        left, *right = _split_strip(args_str, ";")
-        if len(right) > 1:
-            raise BatchScriptParseError(
-                "NULLTOVALUE Error: Too many arguments" + on_line
-            )
-        value = float(right[0])
-        return self._get_raster(left).replace_null(value)
-
-    def _remap_args_to_raster(self, args_str, line_no):
-        on_line = f" on line {line_no}"
-        raster, *args = _split_strip(args_str, ";")
-        if len(args) > 1:
-            raise BatchScriptParseError(
-                "REMAP Error: Too many arguments dividers" + on_line
-            )
-        args = args[0]
-        remaps = []
-        for group in _split_strip(args, ","):
-            try:
-                values = [float(v) for v in _split_strip(group, ":")]
-            except ValueError:
-                raise BatchScriptParseError(
-                    "REMAP Error: values must be numbers" + on_line
-                )
-            if len(values) != 3:
-                raise BatchScriptParseError(
-                    "REMAP Error: requires 3 values separated by ':'" + on_line
-                )
-            left, right, new = values
-            if right <= left:
-                raise BatchScriptParseError(
-                    "REMAP Error: the min value must be less than the max"
-                    " value" + on_line
-                )
-            remaps.append((left, right, new))
-        if len(remaps) == 0:
-            raise BatchScriptParseError(
-                "REMAP Error: No remap values found" + on_line
-            )
-        args = []
-        for group in remaps:
-            args.extend(group)
-        return self._get_raster(raster).remap_range(*args)
-
-    def _composite_args_to_raster(self, args_str, line_no):
-        on_line = f" on line {line_no}"
-        rasters = [
-            self._get_raster(path) for path in _split_strip(args_str, ";")
-        ]
-        if len(rasters) < 2:
-            raise BatchScriptParseError(
-                "COMPOSITE Error: at least 2 rasters are required" + on_line
-            )
-        return rasters[0].band_concat(rasters[1:])
-
-    def _save_args_to_raster(self, args_str, line_no):
-        on_line = f" on line {line_no}"
-        # From c# files:
-        #  (inRaster;outName;outWorkspace;rasterType;nodata;blockwidth;blockheight)
-        # nodata;blockwidth;blockheight are optional
-        try:
-            in_rs, out_name, out_dir, type_, *extra = _split_strip(
-                args_str, ";"
-            )
-        except ValueError:
-            raise BatchScriptParseError(
-                "SAVEFUNCTIONRASTER Error: Incorrect number of arguments"
-                + on_line
-            )
-        n = len(extra)
-        bwidth = None
-        bheight = None
-        nodata = 0
-        if n >= 1:
-            nodata = float(extra[0])
-        if n >= 2:
-            bwidth = int(extra[1])
-        if n == 3:
-            bheight = int(extra[2])
-        if n > 3:
-            raise BatchScriptParseError(
-                "SAVEFUNCTIONRASTER Error: Too many arguments" + on_line
-            )
-        if type_ not in FTYPE_TO_EXT:
-            raise BatchScriptParseError(
-                "SAVEFUNCTIONRASTER Error: Unknown file type" + on_line
-            )
-        raster = self._get_raster(in_rs)
-        out_name = os.path.join(out_dir, out_name)
-        ext = FTYPE_TO_EXT[type_]
-        out_name += f".{ext}"
-        return raster.save(out_name, nodata, bwidth, bheight)
-
-    def _get_raster(self, name_or_path):
+    def get_raster(self, name_or_path):
         if name_or_path in self.rasters:
             return self.rasters[name_or_path]
         else:
@@ -1516,3 +1471,31 @@ class BatchScript:
                 name_or_path = os.path.join(self.location, name_or_path)
             validate_file(name_or_path)
             return Raster(name_or_path)
+
+
+def _parse_batch_script(path):
+    state = _BatchScripParserState(path)
+    with open(state.path) as fd:
+        lines = fd.readlines()
+    last_raster = None
+    for i, line in enumerate(lines):
+        # Ignore comments
+        line, *_ = _split_strip(line, "#")
+        if not line:
+            continue
+        lh, rh = _split_strip(line, "=")
+        state.rasters[lh] = _parse_raster(state, lh, rh, i + 1)
+        last_raster = lh
+    state.final_raster = state.rasters[last_raster]
+    return state
+
+
+def _parse_raster(state, dst, expr, line_no):
+    mat = _FUNC_PATTERN.match(expr)
+    if mat is None:
+        _batch_error("Could not parse function on line", line_no)
+    func = mat["func"].upper()
+    args = mat["args"]
+    if func not in _FUNC_TO_PARSER:
+        _batch_error(f"Unknown function {repr(func)}", line_no)
+    return _FUNC_TO_PARSER[func](state, args, line_no)
