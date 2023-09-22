7f8e7b2eef88fad1074ad7ed5f9399c3952c1dd2
Fred Bunt <fredrick.bunt@gmail.com>
2023-09-22 15:06:37 -0600

    Add module for providing test data, remove unused, rename DEMs to dem

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -4,6 +4,7 @@ import numpy as np
 from affine import Affine
 
 from raster_tools import Raster, distance
+from tests import testdata
 from tests.utils import assert_valid_raster
 
 # Example taken from ESRI docs
@@ -171,21 +172,21 @@ class TestCostDist(unittest.TestCase):
         with self.assertRaises(ValueError):
             # Must have same shape
             distance.cost_distance_analysis(
-                self.cs, "tests/data/raster/elevation_small.tif"
+                self.cs, "tests/data/raster/dem_small.tif"
             )
         with self.assertRaises(TypeError):
             # source raster must be int
             distance.cost_distance_analysis(
-                "tests/data/raster/elevation_small.tif",
-                "tests/data/raster/elevation_small.tif",
+                "tests/data/raster/dem_small.tif",
+                "tests/data/raster/dem_small.tif",
             )
         with self.assertRaises(ValueError):
             # source raster must have null value
             distance.cost_distance_analysis(
-                "tests/data/raster/elevation_small.tif",
-                Raster("tests/data/raster/elevation_small.tif")
-                .astype(int, False)
-                .set_null_value(None),
+                "tests/data/raster/dem_small.tif",
+                testdata.raster.dem_small.astype(int, False).set_null_value(
+                    None
+                ),
             )
         with self.assertRaises(ValueError):
             # sources array must have shape (M, 2)
@@ -220,7 +221,7 @@ class TestCostDist(unittest.TestCase):
 
 
 def test_cost_distance_analysis_crs():
-    rs = Raster("tests/data/raster/elevation_small.tif")
+    rs = testdata.raster.dem_small
     srcs = np.array([[1, 1], [20, 30]])
     cd, tr, al = distance.cost_distance_analysis(rs, srcs)
     assert_valid_raster(cd)
