8aad11d457f8ebe81480f9e48c1faf2d350f0659
Fred Bunt <fredrick.bunt@gmail.com>
2024-12-03 18:14:33 -0700

    Add flag for numpy>=2

--- raster_tools/_compat.py
+++ raster_tools/_compat.py
@@ -1,5 +1,9 @@
 import os
 
+import numpy as np
+
+from raster_tools.utils import version_to_tuple
+
 # Force the use of shapely 2 instead of pygeos in geopandas
 os.environ["USE_PYGEOS"] = "0"
 
@@ -10,3 +14,7 @@ import geopandas as gpd  # noqa: E402
 # backend for line_stats.
 if gpd.options.use_pygeos:
     gpd.options.use_pygeos = False
+
+
+# Numpy 2.0 made several changes to type promotion rules.
+NUMPY_GE_2 = version_to_tuple(np.__version__) >= (2, 0, 0)
